1b4a92e9efd464c8decf63ab9f52d40ad13f1ffd
Andrew Bresee <andrew.p.bresee@gmail.com>
2016-04-26 22:25:22 -0700

    Allow for click events to display what cell was clicked

--- pieceMovements.js
+++ pieceMovements.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-	console.log("READY!")
+	console.log("READY!");
+
+	$('.white').click(function(item){
+		console.log("WHITE CLICKED!: ", item.delegateTarget)
+	})
+
+	$('.black').click(function(item){
+		console.log("BLACK CLICKED!: ", item.delegateTarget)
+	})
 });
