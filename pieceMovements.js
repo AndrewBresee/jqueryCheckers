75fb0c6f8c79eec1a7cfb5668215b5e0dc51d97d
Andrew Bresee <andrew.p.bresee@gmail.com>
2016-04-26 22:40:18 -0700

    Begin rendering pieces to board

--- pieceMovements.js
+++ pieceMovements.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
 	console.log("READY!");
 
+	var $space = $('.white 12');
+	var pieceOne = "<div>HI!</div>";
+
+  var html = $.parseHTML(pieceOne), 
+	// Append the parsed HTML
+	$space.append( html );
+
 	$('.white').click(function(item){
 		console.log("WHITE CLICKED!: ", item.delegateTarget)
 	})
