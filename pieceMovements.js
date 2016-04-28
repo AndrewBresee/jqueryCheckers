fe3818858b1c724db7967fdef9c5a16eec590b08
Andrew Bresee <andrew.p.bresee@gmail.com>
2016-04-27 21:58:52 -0700

    Pass test to get something to append to the table on click

--- pieceMovements.js
+++ pieceMovements.js
@@ -1,18 +1,22 @@
 $(document).ready(function() {
 	console.log("READY!");
 
-	var $space = $('.white 12');
-	var pieceOne = "<div>HI!</div>";
+// 	var $space = $('.white 12');
+// 	var pieceOne = "<div>HI!</div>";
 
-  var html = $.parseHTML(pieceOne), 
-	// Append the parsed HTML
-	$space.append( html );
+//   var html = $.parseHTML(pieceOne);
+// // Append the parsed HTML
 
-	$('.white').click(function(item){
-		console.log("WHITE CLICKED!: ", item.delegateTarget)
+	
+
+
+	$('.white').click(function(){
+		console.log(this)
+		$("<p>Test</p>").appendTo(this);
+	
 	})
 
-	$('.black').click(function(item){
-		console.log("BLACK CLICKED!: ", item.delegateTarget)
+	$('.black').click(function(){
+		console.log(this)
 	})
 });
