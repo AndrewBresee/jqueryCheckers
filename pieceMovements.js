0ae0303dbf5d916127b158dc03512bc9599da7e0
Andrew Bresee <andrew.p.bresee@gmail.com>
2016-04-28 17:49:03 -0700

    Add two player functionality by switching players at each turn

--- pieceMovements.js
+++ pieceMovements.js
@@ -7,31 +7,65 @@ $(document).ready(function() {
 //   var html = $.parseHTML(pieceOne);
 // // Append the parsed HTML
 
+
+
 var selectedPiece; 
 var piecesSpace; 
 var selectedSpace; 
 
+// this will go back and forth. true = red's turn. false = black's turn. 
+var turn = true;
 
 	$('td').click(function(e){
+
 		// finds the class of the selected item
 		// will be useful for when deciding if piece can be moved around 
+
+		// this gives the number of the space
 		selectedSpace = $(this).attr('class').split(' ')[1];
 
 
-		// want to check if the selectedPiece can move to the selectedSpace. 
+		// this checks if the space that was clicked has a piece in it
 		if (this.children.length > 0) {
 			selectedPiece = this.children[0];
-			//sets the pieceSpace so that it can be compared to selected space
+			//sets the piecesSpace so that it can be compared to selected space
 			piecesSpace = $(selectedPiece).parent()[0];
 			piecesSpace = $(piecesSpace).attr('class').split(' ')[1]; 
 		} 
 
-		// if the selectedPieces SPACE - selectedSpace is = 10, then it can move. 
-		if (Math.abs(piecesSpace - selectedSpace) === 10){
-			$(this).append(selectedPiece);
-			console.log("Moved")
+
+		// checks if it is red's turn
+		if (turn) {
+			if ($(selectedPiece).attr('class').split(' ')[0] === 'redP') {
+				// if the selectedPieces SPACE - selectedSpace is = 10, then it can move. 
+				if (selectedSpace - piecesSpace  === 10) {
+					$(this).append(selectedPiece).animate();
+					console.log("Moved")
+					// changes turn 
+					turn = !turn
+				}
+			}
+		} else {
+			if ($(selectedPiece).attr('class').split(' ')[0] === 'backP') {
+				if (piecesSpace - selectedSpace === 10) {
+					$(this).append(selectedPiece).animate();
+					console.log("Moved")
+					// changes turn 
+					turn = !turn
+				}
+			}
 		}
-		
+
+
+
+		// appends the proper turn name
+		if (turn) {
+			$('.playersTurn').empty().append('Red go\'s!')
+		} else {
+			$('.playersTurn').empty().append('Black go\'s!')
+		}
+
+
 		console.log("this :", this); 
 		console.log("selectedPiece :", selectedPiece);
 		console.log("selectedSpace :", selectedSpace); 
